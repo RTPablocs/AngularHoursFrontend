57fcab5ba6a95aaa73c715e34f6ea4ca71bc8ea7
RTPablocs <conejerosorianopablo@gmail.com>
2021-09-16 19:49:56 +0200

    Imported Sidebar Component and Lucide Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LucideAngularModule } from 'lucide-angular';
+import { Clock, BarChart2, ArrowLeft, Award, Printer, Send, User, Plus, Calendar} from 'lucide-angular';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SidebarComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        LucideAngularModule.pick({
+            Clock,
+            BarChart2,
+            ArrowLeft,
+            Award,
+            Printer,
+            Send,
+            User,
+            Plus,
+            Calendar
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
