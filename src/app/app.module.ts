b453183a03ea1de981c2bdaef37514f254094e22
RTPablocs <conejerosorianopablo@gmail.com>
2021-09-17 21:28:23 +0200

    Autogenerated user-form from CLI and imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { LucideAngularModule } from 'lucide-angular';
-import { Clock, BarChart2, ArrowLeft, Award, Printer, Send, User, Plus, Calendar} from 'lucide-angular';
-
+import {
+    Clock,
+    BarChart2,
+    ArrowLeft,
+    Award,
+    Printer,
+    Send,
+    User,
+    Plus,
+    Calendar,
+    Edit,
+    X,
+    Settings
+} from 'lucide-angular';
+import { HttpClientModule } from '@angular/common/http';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { UserComponent } from './pages/user/user.component';
+import { UserFormComponent } from './components/user-form/user-form.component'
 @NgModule({
     declarations: [
         AppComponent,
-        SidebarComponent
+        SidebarComponent,
+        UserCardComponent,
+        UserComponent,
+        UserFormComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
+        HttpClientModule,
         LucideAngularModule.pick({
             Clock,
             BarChart2,
@@ -24,7 +44,10 @@ import { Clock, BarChart2, ArrowLeft, Award, Printer, Send, User, Plus, Calendar
             Send,
             User,
             Plus,
-            Calendar
+            Calendar,
+            Edit,
+            X,
+            Settings
         })
     ],
     providers: [],
