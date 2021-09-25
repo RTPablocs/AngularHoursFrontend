e5ae78c025e5fe64a5dee2649d0702108c5d4014
RTPablocs <conejerosorianopablo@gmail.com>
2021-09-25 20:29:52 +0200

    User update form updated

--- src/app/shared/user-cards/user-cards.component.ts
+++ src/app/shared/user-cards/user-cards.component.ts
@@ -17,10 +17,11 @@ export class UserCardsComponent implements OnInit {
     @Input() user!: commonUser
 
     userForm = new FormGroup({
-        name: new FormControl(''),
-        username: new FormControl(''),
-        mail: new FormControl(''),
-        department: new FormControl('')
+        name: new FormControl(null),
+        surname: new FormControl(null),
+        username: new FormControl(null),
+        mail: new FormControl(null),
+        department: new FormControl(null)
     })
 
 
@@ -32,13 +33,16 @@ export class UserCardsComponent implements OnInit {
     }
 
     submitFormData(): void {
-        console.log(this.userForm.value, this.user.id);
+        this.sanitizeFormOutput()
+        this.userHttp.updateUser(this.user.id, this.userForm.value)
+            .subscribe(data => this.user = data)
         this.userForm.reset()
         this.switchEditMode()
     }
 
-    deleteUser(): void {
-        this.userHttp.deleteOneUser(this.user.id)
+    sanitizeFormOutput(): void {
+        Object.keys(this.userForm.value).forEach((key) => {
+            this.userForm.value[key] == null && delete this.userForm.value[key]
+        })
     }
-
 }
