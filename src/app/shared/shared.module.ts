a5a3f6efb911ba52b0f5ecaf4584ca1a7104d4e3
RTPablocs <conejerosorianopablo@gmail.com>
2021-09-30 09:47:09 +0200

    Added Components from shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { UserButtonsComponent } from './user-buttons/user-buttons.component';
+import { LucideAngularModule } from 'lucide-angular';
+import { Check, Cross, Edit, } from 'lucide-angular';
+import { UserFormComponent } from './user-form/user-form.component';
+import { ClockComponent } from './clock/clock.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserButtonsComponent,
+    UserFormComponent,
+    ClockComponent
+  ],
+  exports: [
+      UserButtonsComponent,
+      UserFormComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    LucideAngularModule.pick({
+        Check,
+        Cross,
+        Edit
+    })
   ]
 })
 export class SharedModule { }
