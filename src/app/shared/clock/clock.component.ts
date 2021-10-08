3b4207f5eb51439c85fef8cac80a8b571d3041bb
RTPablocs <conejerosorianopablo@gmail.com>
2021-10-08 15:38:54 +0200

    Integrated Clock

--- src/app/shared/clock/clock.component.ts
+++ src/app/shared/clock/clock.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, timer } from 'rxjs'
+import { map, share } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-clock',
-  templateUrl: './clock.component.html',
-  styleUrls: ['./clock.component.css']
+    selector: 'app-clock',
+    templateUrl: './clock.component.html',
+    styleUrls: ['./clock.component.css']
 })
-export class ClockComponent implements OnInit {
+export class ClockComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    time = new Date()
+    timeSubscription!: Subscription
+
+    ngOnInit(): void {
+        this.timeSubscription = timer(0, 1000)
+            .pipe(
+                map(() => new Date()),
+                share()
+            ).subscribe(
+                (time) => this.time = time
+            )
+
+    }
+
+    ngOnDestroy(): void {
+        this.timeSubscription.unsubscribe()
+    }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,28 +3,30 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { UserButtonsComponent } from './user-buttons/user-buttons.component';
 import { LucideAngularModule } from 'lucide-angular';
-import { Check, Cross, Edit, } from 'lucide-angular';
+import { Eye, EyeOff } from 'lucide-angular';
 import { UserFormComponent } from './user-form/user-form.component';
 import { ClockComponent } from './clock/clock.component';
+import { PasswordContainerComponent } from './password-container/password-container.component';
 
 
 @NgModule({
   declarations: [
     UserButtonsComponent,
     UserFormComponent,
-    ClockComponent
+    ClockComponent,
+    PasswordContainerComponent
   ],
   exports: [
       UserButtonsComponent,
-      UserFormComponent
+      UserFormComponent,
+      PasswordContainerComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     LucideAngularModule.pick({
-        Check,
-        Cross,
-        Edit
+        Eye,
+        EyeOff
     })
   ]
 })
