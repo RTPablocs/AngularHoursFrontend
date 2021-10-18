e6418ce18e2c7049f30521fd7c9e4ed6051c9374
RTPablocs <conejerosorianopablo@gmail.com>
2021-10-18 15:42:42 +0200

    Added base and day components from cli

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BaseComponent } from './base/base.component';
+import { DayComponent } from './day/day.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BaseComponent,
+    DayComponent
+  ],
   imports: [
     CommonModule
   ]
