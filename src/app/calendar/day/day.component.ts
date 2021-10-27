ef90667f3f2d22aa29d78b5c75f8305c066d5a0c
RTPablocs <conejerosorianopablo@gmail.com>
2021-10-27 19:06:39 +0200

    Full day implementation component

--- src/app/calendar/day/day.component.ts
+++ src/app/calendar/day/day.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
-  selector: 'app-day',
-  templateUrl: './day.component.html',
-  styleUrls: ['./day.component.css']
+    selector: 'app-day',
+    templateUrl: './day.component.html',
+    styleUrls: ['./day.component.css'],
 })
 export class DayComponent implements OnInit {
 
-  constructor() { }
+    today = new Date()
+    @Output() dataToParent = new EventEmitter<any>()
 
-  ngOnInit(): void {
-  }
 
+    constructor() {
+    }
+
+
+    ngOnInit(): void {
+
+    }
+
+    resetDate(): void{
+        this.today = new Date()
+}
+    changeDate(arg: number): void {
+        this.today = new Date(this.today.getFullYear(), this.today.getMonth(), this.today.getDate() + arg)
+    }
+    elevateToParent(arg: any):void {
+        this.dataToParent.emit(arg)
+    }
 }
