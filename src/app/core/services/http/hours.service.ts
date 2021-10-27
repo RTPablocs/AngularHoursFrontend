2288f0085e638b6d256f59f4701e354f01237221
RTPablocs <conejerosorianopablo@gmail.com>
2021-10-27 19:11:27 +0200

    added service to register hours

---
+++ src/app/core/services/http/hours.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core';
+import {ApiService} from "./api.service";
+import {Observable} from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class HoursService {
+
+    constructor(private api: ApiService) {
+    }
+    registerHour(data:any): Observable<any>{
+        return this.api.post('hours/new', data)
+    }
+}
