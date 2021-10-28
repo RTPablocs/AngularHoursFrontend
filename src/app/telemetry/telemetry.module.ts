e8449967767ccdf2494e1ec06760934bfa848bb5
RTPablocs <conejerosorianopablo@gmail.com>
2021-10-28 10:11:02 +0200

    Generated telemetry module and chart component from cli

---
+++ src/app/telemetry/telemetry.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChartBaseComponent } from './chart-base/chart-base.component';
+
+
+
+@NgModule({
+  declarations: [
+    ChartBaseComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TelemetryModule { }
